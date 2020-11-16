2c91c80e3addabe4f76b35ac32cccabdfe53f967
GreenCyborgNinjaDude <kiet.truong98@gmail.com>
2020-11-15 19:42:33 -0800

    Add files via upload
    All of the files should be fixed to the changes in the instruction class

---
+++ src/model/instructionType/ArithmeticShiftLeft.java
@@ -0,0 +1,32 @@
+import controller.Controller;
+import model.instructionType.Instruction;
+
+/**
+ * This class was design to shift
+ * the selected binary number first 1 to the left
+ * WARNING: only UNSIGNED binary! 
+ */
+
+public class ArithmeticShiftLeft extends Instruction{
+	
+	/** The destination register number. */
+	private String myRd;
+   
+	/** Temporary int holder. */
+	public int holder;
+	
+	public  ArithmeticShiftLeft (String myInstructionSpecifier, String operandSpecifier, String myRd) {
+		super(myInstructionSpecifier,operandSpecifier);
+		this.myRd = myRd;
+	}
+	
+	@Override
+	public void execute(Controller theCon) {
+		// TODO Auto-generated method stub
+		holder = Integer.valueOf(myRd);
+		holder = holder >>> 1;
+		
+		theCon.setMyIndexRegister(String.valueOf(holder));
+	}
+    
+}
---
+++ src/model/instructionType/RotateLeft.java
@@ -0,0 +1,30 @@
+import controller.Controller;
+import model.instructionType.Instruction;
+
+/**
+ * This class was design to Rotate
+ * the selected binary number first 1 to the left
+ */
+
+public class RotateLeft extends Instruction{
+
+	/** The destination register number. */
+	private String myRd;
+   
+	/** Temporary array holder. */
+	public String holder;
+	
+	public  RotateLeft (String myInstructionSpecifier, String operandSpecifier, String myRd) {
+		super(myInstructionSpecifier,operandSpecifier);
+		this.myRd = myRd;
+	}
+	
+	@Override
+	public void execute(Controller theCon) {
+		holder = myRd.substring(1) + myRd.substring(0, 1);
+		
+		theCon.setMyIndexRegister(holder);
+	}
+	
+
+}
