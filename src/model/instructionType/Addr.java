a8632c9f209f38d15f979e57d18d7c27a4c94e88
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-11-21 17:58:26 -0800

    subr addr implement

---
+++ src/model/instructionType/Addr.java
@@ -0,0 +1,36 @@
+package model.instructionType;
+
+import model.Converter;
+import controller.Controller;
+
+/**
+ * This class is the add instruction
+ */
+
+public class Addr extends Instruction {
+
+	public Addr(String myInstructionSpecifier, String operandSpecifier) {
+		super(myInstructionSpecifier, operandSpecifier);
+	}
+
+	@Override
+	public void execute(Controller theCon) {
+
+		String addressingMode = super.getMyInstructionSpecifier().substring(4);
+		int ac =  Converter.binToDec(theCon.getMyAccumulatorRegister());
+		int operand = Converter.binToDec(super.getMyOperandSpecifier());
+
+		if (addressingMode.equals("0000")) { // Immediate value,ac
+			int result = ac + operand;
+			theCon.setMyAccumulatorRegister(Converter.decToBin(result,16));
+		} else if (addressingMode.equals("1000")) {  // Immediate value,index register
+			int result = ac + operand;
+			theCon.setMyIndexRegister(Converter.decToBin(result,16));
+		} else { //memory,direct
+			int ir = Converter.binToDec(theCon.getMyIndexRegister());
+			int result = ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand));
+			theCon.setMyAccumulatorRegister(Converter.decToBin(result,16));
+		}
+
+	}
+}
---
+++ src/model/instructionType/Subr.java
@@ -0,0 +1,37 @@
+package model.instructionType;
+
+import controller.Controller;
+import model.Converter;
+
+/**
+ * This class is the subtract instruction
+ */
+
+public class Subr extends Instruction {
+	public Subr(String myInstructionSpecifier, String myOperandSpecifier) {
+		super(myInstructionSpecifier, myOperandSpecifier);
+	}
+
+	@Override
+	public void execute(Controller theCon) {
+		String addressingMode = super.getMyInstructionSpecifier().substring(4);
+		int ac =  Converter.binToDec(theCon.getMyAccumulatorRegister());
+		int operand = Converter.binToDec(super.getMyOperandSpecifier());
+
+		if (addressingMode.equals("0000")) { // Immediate value,ac
+			int result = ac - operand;
+			theCon.setMyAccumulatorRegister(Converter.decToBin(result,16));
+		} else if (addressingMode.equals("1000")) {  // Immediate value,index
+			int result = ac - operand;
+			theCon.setMyIndexRegister(Converter.decToBin(result,16));
+		} else if (addressingMode.equals("0001")) { // ac addressing
+			int ir = Converter.binToDec(theCon.getMyIndexRegister());
+			int result = ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand));
+			theCon.setMyAccumulatorRegister(Converter.decToBin(result,16));
+		}else{
+			int ir = Converter.binToDec(theCon.getMyIndexRegister());
+			int result = ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand));
+			theCon.setMyIndexRegister(Converter.decToBin(result,16));
+		}
+	}
+}
