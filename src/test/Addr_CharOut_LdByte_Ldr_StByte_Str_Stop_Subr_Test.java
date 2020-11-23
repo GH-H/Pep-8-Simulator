0d21b624cd8924e3c228e7328c68efb8a7eba767
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-11-22 20:03:24 -0800

    bug fix

--- src/test/Addr_CharOut_LdByte_Ldr_StByte_Str_Stop_Subr_Test.java
+++ src/test/Addr_CharOut_LdByte_Ldr_StByte_Str_Stop_Subr_Test.java
@@ -9,15 +9,27 @@ public class Addr_CharOut_LdByte_Ldr_StByte_Str_Stop_Subr_Test {
     @Test
     public void testadd() {
         Controller controller = new Controller();
-        Addr addA = new Addr("01110000","0000000000001010"); // AddA 32778,i
+        Addr a = new Addr("01110000","0000000000001010"); // AddA 10,i
 
-        // Artificially setup Accumulator to hold a value
         controller.setMyAccumulatorRegister("0000000000000100");
 
-        // Execute and confirm that "1010" AND "1100" = "1000" for rightmost 4 bits in Operand Specifier.
-        // Also confirm that N flag was set to 1.
-        addA.execute(controller);
+
+        a.execute(controller);
         assertEquals(controller.getMyAccumulatorRegister(), "0000000000001110");
+        assertEquals(controller.getMyOperand(), "0000000000001010");
+
+    }
+
+    @Test
+    public void testsub() {
+        Controller controller = new Controller();
+        Subr a = new Subr("10000000","0000000000001010"); // a 10,i
+
+
+        controller.setMyAccumulatorRegister("0000000000000010");
+
+        a.execute(controller);
+        assertEquals(controller.getMyAccumulatorRegister(), "0000000000001000");
         assertEquals(controller.getMyOperand(), "0000000000001010");
 
     }
