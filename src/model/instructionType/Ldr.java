5e387f68bc528d9efe29b9f55bb95969545b7cfc
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-11-22 18:15:17 -0800

    ld flags update

--- src/model/instructionType/Ldr.java
+++ src/model/instructionType/Ldr.java
@@ -18,7 +18,23 @@ public class Ldr extends Instruction {
 	public void execute(Controller theCon) {
 		theCon.setMyOperand(super.getMyOperandSpecifier());
 		int address = Converter.binToDec(super.getMyOperandSpecifier());
+		String result = theCon.getMyMemoryDataAt(address)+ theCon.getMyMemoryDataAt(address+1);
 
-		theCon.setMyAccumulatorRegister(theCon.getMyMemoryDataAt(address)+ theCon.getMyMemoryDataAt(address+1));
+		theCon.setMyAccumulatorRegister(result);
+
+		theCon.setMyNFlag(NFlag(result));
+		theCon.setMyZFlag(ZFlag(result));
+	}
+
+	private int NFlag(final String theBinary) {
+		return Integer.parseInt(theBinary.substring(0,1));
+	}
+
+	private int ZFlag(final String theBinary) {
+		int output = 0;
+		if (theBinary.indexOf("1") == -1) { // There are no 1's in the binary string.
+			output = 1;
+		}
+		return output;
 	}
 }
