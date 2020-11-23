69d5edc9c3a81efe0d286fcb55bd7ca50de13e57
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-11-22 17:55:29 -0800

    Ldr fix

--- src/model/instructionType/Ldr.java
+++ src/model/instructionType/Ldr.java
@@ -8,9 +8,9 @@ import model.Converter;
  * This class is the LW instruction
  */
 
-public class LW extends Instruction {
+public class Ldr extends Instruction {
 
-	public LW(String myInstructionSpecifier, String myOperandSpecifier) {
+	public Ldr(String myInstructionSpecifier, String myOperandSpecifier) {
 		super(myInstructionSpecifier, myOperandSpecifier);
 	}
 
@@ -19,6 +19,6 @@ public class LW extends Instruction {
 		theCon.setMyOperand(super.getMyOperandSpecifier());
 		int address = Converter.binToDec(super.getMyOperandSpecifier());
 
-		theCon.setMyAccumulatorRegister(theCon.getMyMemoryDataAt(address));
+		theCon.setMyAccumulatorRegister(theCon.getMyMemoryDataAt(address)+ theCon.getMyMemoryDataAt(address+1));
 	}
 }
