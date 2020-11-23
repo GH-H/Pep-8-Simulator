83f71e84a2917c83916e1b01ae26ae45ce19a548
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-11-22 19:18:15 -0800

    addr,subr flags update

--- src/model/instructionType/Subr.java
+++ src/model/instructionType/Subr.java
@@ -18,21 +18,56 @@ public class Subr extends Instruction {
 		int ac =  Converter.binToDec(theCon.getMyAccumulatorRegister());
 		int operand = Converter.binToDec(super.getMyOperandSpecifier());
 		theCon.setMyOperand(super.getMyOperandSpecifier());
-
+		String result;
 		if (addressingMode.equals("0000")) { // Immediate value,ac
-			int result = ac - operand;
-			theCon.setMyAccumulatorRegister(Converter.decToBin(result,16));
+			result = Converter.decToBin(ac - operand,16);
+			theCon.setMyAccumulatorRegister(result);
 		} else if (addressingMode.equals("1000")) {  // Immediate value,index
-			int result = ac - operand;
-			theCon.setMyIndexRegister(Converter.decToBin(result,16));
+			result = Converter.decToBin(ac - operand,16);
+			theCon.setMyIndexRegister(result);
 		} else if (addressingMode.equals("0001")) { // ac addressing
 			int ir = Converter.binToDec(theCon.getMyIndexRegister());
-			int result = ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand));
-			theCon.setMyAccumulatorRegister(Converter.decToBin(result,16));
+			result = Converter.decToBin(ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand)),16);
+			theCon.setMyAccumulatorRegister(result);
 		}else{
 			int ir = Converter.binToDec(theCon.getMyIndexRegister());
-			int result = ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand));
-			theCon.setMyIndexRegister(Converter.decToBin(result,16));
+			result = Converter.decToBin(ir + Converter.binToDec(theCon.getMyMemoryDataAt(operand)),16);
+			theCon.setMyIndexRegister(result);
 		}
+
+		theCon.setMyNFlag(NFlag(result));
+		theCon.setMyZFlag(ZFlag(result));
+		theCon.setMyVFlag(VFlag(ac,operand,Converter.binToDec(result)));
+		theCon.setMyCFlag(CFlag(Converter.decToBin(ac,16),Converter.decToBin(operand,16),result));
+	}
+
+	private int NFlag(final String theBinary) {
+		return Integer.parseInt(theBinary.substring(0,1));
+	}
+
+	private int ZFlag(final String theBinary) {
+		int output = 0;
+		if (theBinary.indexOf("1") == -1) { // There are no 1's in the binary string.
+			output = 1;
+		}
+		return output;
+	}
+
+	private int VFlag(int o1, int o2,int result) {
+
+		if((o1>0&&o2>0&&result<0)||(o1<0&&o2<0&&result>0)){
+			return 1;
+		}
+		return 0;
+	}
+
+	private int CFlag(String o1,String o2,String result){
+		int o1b = Integer.parseInt(o1.substring(0,1));
+		int o2b = Integer.parseInt(o1.substring(0,1));
+		int resultB = Integer.parseInt(o1.substring(0,1));
+		if((o1b==1&&o1b==1&&resultB==0)||(o1b==0&&o1b==0&&resultB==1)){
+			return 1;
+		}
+		return 0;
 	}
 }
